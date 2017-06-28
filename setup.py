a94d8ee34f1ee95e40ee893d6cbfd9b1401ba93e
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-28 08:30:53 -0400

    added package setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='wolfgang',
+      version='0.1',
+      description='A Python library for vectors.',
+      url='https://github.com/Cisplatin/wolfgang',
+      author='Simon Hajjar',
+      license='MIT',
+      packages=['wolfgang'],
+      zip_safe=False)
