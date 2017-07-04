217c11f9dbb049697af58ba05f87fc2794faef8e
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-07-04 14:53:19 -0400

    added real_basis for span

--- wolfgang/span.py
+++ wolfgang/span.py
@@ -8,3 +8,17 @@ class Span:
   # @return [Integer] The number of vectors in the spanning set.
   def __len__(self):
     return len(self.vectors)
+
+  # @param dimension [Integer] The dimension of the real space to use.
+  # @return [Span] A basis for R^(dimension).
+  # @raise [ValueError] If dimension is non-positive.
+  @staticmethod
+  def real_basis(dimension):
+    if dimension <= 0:
+      raise ValueError('Dimension of span must be positive.')
+    vectors = []
+    for i in xrange(dimension):
+      vector = Vector([0] * dimension)
+      vector[i] = 1
+      vectors.append(vector)
+    return Span(vectors)
--- wolfgang/tests/test_span.py
+++ wolfgang/tests/test_span.py
@@ -9,9 +9,14 @@ class TestSpanClass(unittest.TestCase):
     span = Span([Vector.unit_vector(3), Vector([1, 2, 3])])
     self.assertEqual(span.vectors, [Vector.unit_vector(3), Vector([1, 2, 3])])
 
-  def test_init(self):
+  def test_len(self):
     span = Span([Vector.unit_vector(3), Vector([1, 2, 3])])
     self.assertEqual(len(span), 2)
+
+  def test_real_basis(self):
+    span = Span.real_basis(3)
+    basis = [Vector([1, 0, 0]), Vector([0, 1, 0]), Vector([0, 0, 1])]
+    self.assertEqual(span.vectors, basis)
 
 if __name__ == '__main__':
   unittest.main()
