2d856117e317c310cf1399615233cb4c840ae096
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-07-04 14:16:49 -0400

    added author_email to package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='wolfgang',
       description='A Python library for vectors.',
       url='https://github.com/Cisplatin/wolfgang',
       author='Simon Hajjar',
+      author_email='simon.j.hajjar@gmail.com',
       license='MIT',
       packages=['wolfgang'],
       zip_safe=False)
